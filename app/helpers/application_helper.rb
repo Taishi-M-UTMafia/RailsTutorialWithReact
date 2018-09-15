b7969ba66948c745d1fd3d798932f23d054062c9
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-09-15 13:47:02 +0900

    finish chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   # ページ毎の完全なタイトルを返す
   def full_title(page_title = '')
     base_title = 'Ruby on Rails Tutorial Sample App'
-    page_title.empty? ? base_title : page_title + '|' + base_title
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # helperのメソッドもテストで使えるように追加
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
